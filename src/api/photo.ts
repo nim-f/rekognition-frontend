8c290135bb286742a33c52daa34c3a81154c69ef
Анна Шахбазян <fatamorgana9205@gmail.com>
2022-12-30 19:51:22 +0000

    add env variable for backend url

--- src/api/photo.ts
+++ src/api/photo.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3009";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3009";
 
 export const uploadPhoto = (body: FormData) =>
     fetch(API_URL, {
