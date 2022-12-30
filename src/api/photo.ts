f1cc1eb3c32d1f9d988ca6e766a749ff58ed558e
Анна Шахбазян <fatamorgana9205@gmail.com>
2022-12-30 19:56:25 +0000

    debug api url

--- src/api/photo.ts
+++ src/api/photo.ts
@@ -1,5 +1,7 @@
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3009";
 
+console.log({ API_URL });
+
 export const uploadPhoto = (body: FormData) =>
     fetch(API_URL, {
         method: "POST",
